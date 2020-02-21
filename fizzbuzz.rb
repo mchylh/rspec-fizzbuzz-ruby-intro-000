e4cb9d4826c86d333218c3933135ff1cddb873cf
Michaylah Mam <mchylh@gmail.com>
2020-02-21 18:04:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizz_3[fizzbuzz(number)]
+def fizzbuzz(number)
   number % 3 == 0
     "Fizz"
+    fizz_3 = fizzbuzz(3)
 end
