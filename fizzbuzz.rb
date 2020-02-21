4c627d8f00ab5c0002266672edff85d7fd2c21d5
Michaylah Mam <mchylh@gmail.com>
2020-02-21 18:18:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
+require_relative './spec_helper.rb'
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   if (number % 3 == 0) && (number % 5 == 0)
